4c951d8145b366c94fc2e1408e99d5edf54055cd
Ivanna86Koval <125496314+Ivanna86Koval@users.noreply.github.com>
2023-10-03 06:31:57 +0000

    Deploying to gh-pages from @ Ivanna86Koval/goit-react-hw-05-movies@60ff8125ea8899b5cc307509c0f046061e3c50e2 🚀

---
+++ static/js/75.017484f8.chunk.js
@@ -0,0 +1,2 @@
+(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[75],{111:function(){},75:function(e,t,n){"use strict";n.r(t);n(791);var r=n(111),u=n(184);t.default=function(){return(0,u.jsx)(r.MovieList,{listType:"trending"})}}}]);
+//# sourceMappingURL=75.017484f8.chunk.js.map
